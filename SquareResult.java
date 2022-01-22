a494a05a01678c1b012e7340119dbf27aa207bed
Vlad Stepanchev <55149963+7sug@users.noreply.github.com>
2022-01-22 08:42:45 +0300

    Delete SquareResult.java

--- SquareResult.java
+++
@@ -1,9 +0,0 @@
-public class SquareApplication {
-    public static void main(String[] args) {
-        double root;
-        double myroot;
-        root = 1048576;
-        myroot = SquareRoot.sqrtRoot(root);
-        System.out.println("Мой расчет корня: " + myroot);
-    }
-}
