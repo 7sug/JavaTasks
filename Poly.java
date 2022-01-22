78d1478bb88d72bda076577d1af7357c64ba949e
Vlad Stepanchev <55149963+7sug@users.noreply.github.com>
2022-01-22 08:56:04 +0300

    Update and rename Polynoms.java to Poly.java

---
+++ Poly.java
@@ -0,0 +1,5 @@
+public class Poly {
+    public static void main(String[] args){
+        Calculation.calc(Data.adaptData(Parsing.parsing(args)));
+    }
+}
--- Polynoms.java
+++
@@ -1,5 +0,0 @@
-public class Polynoms {
-    public static void main(String[] args){
-        CalcPoly.calcpoly(AdaptData.adaptData(Parsing.parsing(args)));
-    }
-}
