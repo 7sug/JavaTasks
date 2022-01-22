448fdc4839aa6f9e6c6b94071388ae9cb190b87f
Vlad Stepanchev <55149963+7sug@users.noreply.github.com>
2022-01-22 08:44:42 +0300

    Create SquareResult.java

---
+++ SquareResult.java
@@ -0,0 +1,9 @@
+public class SquareResult {
+    public static void main(String[] args) {
+        double root;
+        double ResRoot;
+        root = 1048576;
+        ResRoot = Square.sqrtRoot(root);
+        System.out.println("Корень: " + ResRoot);
+    }
+}
